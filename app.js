cd009ab14ee2a16a95a0e1d40c0e9a87ef1f92e3
Marcus Peterson <mdabomb5@gmail.com>
2019-08-05 15:41:12 -0600

    added color

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let container = document.getElementById('container')
+let colorful = false;
 
 function drawGrid (height, width){
     let quantity = height * width;
@@ -7,7 +8,34 @@ function drawGrid (height, width){
     for(let i = 0; i < quantity; i++){
         let div = document.createElement('div')
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black'
+            if(colorful){
+                let color = Math.floor(Math.random() * 16777216).toString(16);
+                let final = '#000000'.slice(0, -color.length) + color;
+                if(div.style.backgroundColor){
+                    if(div.style.backgroundColor[0] === '#'){
+                        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(div.style.backgroundColor);
+                        let rgb = result ? { r: parseInt(result[1], 16),
+                            g: parseInt(result[2], 16),
+                            b: parseInt(result[3], 16)
+                        } : null;
+                        let {r,g,b} = rgb;
+                        r -= 5
+                        g -= 5
+                        b -= 5
+                        div.style.backgroundColor = `rgb(${r},${g},${b})`
+                    }else {
+                        let bc = div.style.backgroundColor;
+                        let nums = bc.slice(bc.indexOf('(') +1, bc.indexOf(')'))
+                        nums = nums.split(', ')
+                        div.style.backgroundColor = `rgb(${nums[0] -= 50}, ${nums[1] -= 50}, ${nums[2] -= 50})`
+                    }
+                }else {
+                    div.style.backgroundColor = final;
+                }
+            }else {
+                div.style.backgroundColor = 'black';
+            }
+            
         })
         container.appendChild(div)
     }
@@ -20,4 +48,11 @@ clear.addEventListener('click', ()=> {
     let width = prompt('How many pixels wide do you want? (10-200) ')
     let height = prompt('How many pixels tall? (10-200) ')
     drawGrid(height, width)
+})
+
+let color = document.getElementById('color')
+
+color.addEventListener('click', ()=> {
+    color.textContent === 'Colorful' ? color.textContent = 'Black' : color.textContent = 'Colorful';
+    colorful = !colorful;
 })
